44a91a2aa770184f38d6bb6cd33db9b8d923391d
delovy2022 <111706070+delovy2022@users.noreply.github.com>
2022-09-08 20:03:26 +0300

    Created functions in js to get real data

--- src/app.js
+++ src/app.js
@@ -20,23 +20,37 @@ function formateDate(timestamp) {
   }
   return `${day} ${hours}:${minutes}`;
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
   console.log(response.data.daily);
   let forecastElement = document.querySelector(`#forecast`);
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday"];
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  days.forEach(function (forecast, index) {
+    if (index < 6)
+      forecastHTML =
+        forecastHTML +
+        `
    <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
-  <img src="http://openweathermap.org/img/wn/03d@2x.png" alt="" width="42" />
+            <div class="weather-forecast-date">${formatDay(
+              forecastDay.dt
+            )}</div>
+  <img src="http://openweathermap.org/img/wn/${
+    forecastDay.weather[0].icon
+  }@2x.png" alt="" width="42" />
   <div class="weather-forecast-temperatures">
   <span class="weather-forecast-temperatures-max">
-  24°C/</span>
-  <span class="weather-forecast-temperatures-min">15°C</span></div>
+  ${Math.round(forecastDay.temp.max)}°C / </span>
+  <span class="weather-forecast-temperatures-min">${Math.round(
+    forecastDay.temp.min
+  )}°C</span></div>
   </div>
   `;
   });
@@ -44,12 +58,14 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = `c95d60a1e3adbeb286133f1ebebc2579`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+
 function showTemperature(response) {
   celsiusTemperature = response.data.main.temp;
   let currentTemperature = Math.round(celsiusTemperature);
@@ -76,17 +92,20 @@ function showTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
   getForecast(response.data.coord);
 }
+
 function search(city) {
   let apiKey = `c95d60a1e3adbeb286133f1ebebc2579`;
   let units = `metric`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
 }
+
 function handleSearch(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector(`#exampleInputlocation`).value;
   search(cityInputElement);
 }
+
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector(`#tempa`);
@@ -95,6 +114,7 @@ function displayFahrenheitTemperature(event) {
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
+
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector(`#tempa`);
@@ -102,6 +122,7 @@ function displayCelsiusTemperature(event) {
   fahrenheitLink.classList.remove("active");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
+
 let celsiusTemperature = null;
 let form = document.querySelector(`#search-form`);
 form.addEventListener(`submit`, handleSearch);
